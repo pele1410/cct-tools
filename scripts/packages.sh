ec578160102d9be5d9def11c0d5611b76e11f4b9
Christopher Thompson <pele1410@gmail.com>
2022-02-20 05:58:24 -0600

    Add special installation for MS tools

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -16,3 +16,16 @@ sudo dnf install \
      thunderbird \
      tmux \
      -y
+
+# Until MS provides .repo files from a well-known URL,
+# install packages from existing version and then just
+# update
+sudo dnf install \
+     https://packages.microsoft.com/yumrepos/vscode/code-1.64.2-1644445844.el7.x86_64.rpm \
+     https://packages.microsoft.com/yumrepos/ms-teams/teams-1.4.00.7556-1.x86_64.rpm \
+     -y
+
+sudo dnf update \
+     code \
+     teams \
+     -y
