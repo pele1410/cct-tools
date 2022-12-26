a6404ed2a0813858cb3326c1b61f00bfef54dd4f
Christopher Thompson <pele1410@gmail.com>
2022-12-26 07:25:26 -0600

    Add shell execution and gdb shortcuts

--- home/.emacs.d/init-shortcuts.el
+++ home/.emacs.d/init-shortcuts.el
@@ -1,8 +1,14 @@
 ;; Shortcut to compile
-(global-set-key "\C-x\C-m" 'compile)
+(global-set-key (kbd "C-x C-m") 'compile)
+
+;; Shortcut to GDB shell command
+(global-set-key (kbd "S-<f5>") 'gdb)
+
+;; Shortcut to execute shell command
+(global-set-key (kbd "<f5>") 'shell-command)
 
 ;; Shortcut to ag search
-(global-set-key "\C-x\C-a" 'ag)
+(global-set-key (kbd "C-x C-a") 'ag)
 
 ;; Shortcut to search and replace
-(global-set-key "\C-x\C-r" 'replace-string)
+(global-set-key (kbd "C-x C-r") 'replace-string)
