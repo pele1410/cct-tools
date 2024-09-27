c19f0a4a1a816cdfe97c31f9019415f5c9eacd6d
Christopher Thompson <pele1410@gmail.com>
2024-09-27 06:18:49 -0500

    Add negativo repo to list of repositories

--- scripts/install-repositories.sh
+++ scripts/install-repositories.sh
@@ -14,5 +14,8 @@ sudo yum install \
 sudo dnf install 'dnf-command(config-manager)'
 sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
 
+# Install Negativo for NVIDIA/CUDA
+sudo dnf config-manager --add-repo=https://negativo17.org/repos/fedora-nvidia.repo
+
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
