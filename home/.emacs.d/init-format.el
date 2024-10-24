607348b86bdde92c824696770213dca105f77c0b
Christopher Thompson <cthompson@metalsharkboats.com>
2024-10-24 11:41:28 -0500

    HACK: workaround emacs clang-format on save problem

--- home/.emacs.d/init-format.el
+++ home/.emacs.d/init-format.el
@@ -1,8 +1,12 @@
-;; Untabify convenience
+;; Always Untabify
 (defun untabify-buffer ()
   "Untabify current buffer"
   (interactive)
   (untabify (point-min) (point-max)))
+(add-hook 'before-save-hook 'untabify-buffer)
+
+;; Delete trailing whitespace before saving
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 ;; Set standard indent to 2 for C++ and CUDA
 (defun my-c++-mode-hook ()
@@ -11,25 +15,28 @@
   (setq-local tab-width 2)
   (setq-local c-basic-indent 2)
   (setq-local c-default-style "linux" c-basic-offset 2)
-  (setq-local indent-tabs-mode 1)
-  (require 'clang-format)
-  (add-hook 'before-save-hook
-            'clang-format-buffer nil)
-  (add-hook 'before-save-hook
-            'untabify-buffer nil))
+  (setq-local indent-tabs-mode 1))
 
 (add-hook 'c-mode-hook 'my-c++-mode-hook)
-(add-hook 'c++-mode-hook 'my-c++-mode-hook)
+(add-hook 'c++-mode-hook  'my-c++-mode-hook)
 (add-hook 'cuda-mode-hook 'my-c++-mode-hook)
+
+;; I do not know why having this 'before-save-hook added as part
+;; of the my-c++-mode-hook function does not work.  To simply
+;; get it working just add individually for each mnode
+(add-hook 'c-mode-hook (add-hook 'before-save-hook
+            'clang-format-buffer))
+(add-hook 'c++-mode-hook  (add-hook 'before-save-hook
+            'clang-format-buffer))
+(add-hook 'cuda-mode-hook (add-hook 'before-save-hook
+            'clang-format-buffer))
 
 ;; Set standard indent to 4 for python
 (defun my-python-mode-hook ()
   (setq-local standard-indent 4)
   (setq-local default-tab-width 4)
   (setq-local tab-width 4)
-  (setq-local indent-tabs-mode 1)
-  (add-hook 'before-save-hook
-            'untabify-buffer nil))
+  (setq-local indent-tabs-mode 1))
 (add-hook 'python-mode-hook 'my-python-mode-hook)
 
 ;; Set standard indent to 2 for xml
@@ -37,17 +44,11 @@
   (setq-local standard-indent 2)
   (setq-local default-tab-width 2)
   (setq-local tab-width 2)
-  (setq-local indent-tabs-mode 1)
-  (add-hook 'before-save-hook
-            'untabify-buffer nil))
+  (setq-local indent-tabs-mode 1))
 (add-hook 'nxml-mode-hook 'my-xml-mode-hook)
 
 ;; Enable clang-format
 (global-set-key (kbd "C-M-]") 'clang-format-buffer)
 
-;; Delete trailing whitespace before saving
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
-
 ;; Ask for a newline at the end of the file
 (setq-local require-final-newline 'ask)
-
